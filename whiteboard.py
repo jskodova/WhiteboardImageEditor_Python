9e82c836e777f37a9e3b78be2d6e99163878e90e
jskodova <skodovajarmilatt@gmail.com>
2023-07-10 21:39:11 +0300

    added side menu with color palette

--- whiteboard.py
+++ whiteboard.py
@@ -12,6 +12,57 @@ root.geometry("1200x700+150+50")
 root.config(bg="#f2f3f5")
 root.resizable(width=True, height=True)
 
+
+# Sidemenu
+side_menu = PhotoImage(file="img/side_menu.png")
+Label(root, image=side_menu, bg="#f2f3f5").place(x=10, y=20)
+
+eraser = PhotoImage(file="img/eraser_1.png")
+Button(root, image=eraser, bg="#f2f3f5", command=new_canvas).place(x=30, y=400)
+
+add_image = PhotoImage(file="img/addimage_1.png")
+Button(root, image=add_image, bg="white", command=insert_image).place(x=30, y=450)
+
+# Color palette for sidemenu
+colors = Canvas(root, width=37, height=300, bg="white", bd=0)
+colors.place(x=30, y=60)
+
+def display_palette():
+    id = colors.create_rectangle(10, 10, 30, 30, fill="black")
+    colors.tag_bind(id, "<Button-1>", lambda x: showcolor("black"))
+
+    id = colors.create_rectangle(10, 40, 30, 60, fill="gray")
+    colors.tag_bind(id, "<Button-1>", lambda x: showcolor("gray"))
+
+    id = colors.create_rectangle(10, 70, 30, 90, fill="brown")
+    colors.tag_bind(id, "<Button-1>", lambda x: showcolor("brown"))
+
+
+    id = colors.create_rectangle(10, 100, 30, 120, fill="red")
+    colors.tag_bind(id, "<Button-1>", lambda x: showcolor("red"))
+
+
+    id = colors.create_rectangle(10, 130, 30, 150, fill="orange")
+    colors.tag_bind(id, "<Button-1>", lambda x: showcolor("orange"))
+
+
+    id = colors.create_rectangle(10, 160, 30, 180, fill="yellow")
+    colors.tag_bind(id, "<Button-1>", lambda x: showcolor("yellow"))
+
+
+    id = colors.create_rectangle(10, 190, 30, 210, fill="green")
+    colors.tag_bind(id, "<Button-1>", lambda x: showcolor("green"))
+
+
+    id = colors.create_rectangle(10, 220, 30, 240, fill="blue")
+    colors.tag_bind(id, "<Button-1>", lambda x: showcolor("blue"))
+
+    id = colors.create_rectangle(10, 250, 30, 270, fill="purple")
+    colors.tag_bind(id, "<Button-1>", lambda x: showcolor("purple"))
+
+display_palette()
+
+
 # Main Window
 canvas = Canvas(root, width=1070, height=620, bg="white", cursor="hand2")
 canvas.place(x=100, y=50)
