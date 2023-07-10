5746aec034b45f20931c7effff05c777c5157f7a
jskodova <skodovajarmilatt@gmail.com>
2023-07-10 21:38:17 +0300

    added tkinter initialization and main window

--- whiteboard.py
+++ whiteboard.py
@@ -6,3 +6,18 @@ from tkinter import filedialog
 from PIL import Image, ImageOps, ImageFilter, ImageTk
 import os
 
+root = Tk()
+root.title("Whiteboard")
+root.geometry("1200x700+150+50")
+root.config(bg="#f2f3f5")
+root.resizable(width=True, height=True)
+
+# Main Window
+canvas = Canvas(root, width=1070, height=620, bg="white", cursor="hand2")
+canvas.place(x=100, y=50)
+
+canvas.bind("<Button-1>", locate_xy)
+
+canvas.bind("<B1-Motion>", addline)
+
+root.mainloop()
