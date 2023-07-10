2e59a3c856f365c78f1c77dd7c66764ca7114019
jskodova <skodovajarmilatt@gmail.com>
2023-07-10 21:37:36 +0300

    added imports

---
+++ whiteboard.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter.colorchooser import askcolor
+from tkinter import ttk
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageOps, ImageFilter, ImageTk
+import os
+
